4a0901427bbe268c525a0827fe7bdf38c09144e0
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2024-09-29 21:12:13 +0500

    .

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 var container = document.getElementById("container-div");
 var toggleBtn = document.getElementById("btn");
-toggleBtn.addEventListener('click', () => {
-    for (var i = 0; i < 4;i++){
-        for(var j = 0 ; j<4; j++){
+toggleBtn.addEventListener('click', () => container.classList.toggle('big'))
+function myFUnc() {
+    for (var i = 0; i < 4; i++) {
+        for (var j = 0; j < 4; j++) {
             var box = document.createElement('div');
             box.classList.add('boxes')
-            boxes.style.backgroundPosition=`${j*125}px${i*125}px`
+            boxes.style.backgroundPosition = `${j * 125}px${i * 125}px`
             container.appendChild(boxes)
         }
     }
-})
+}
+myFUnc()
