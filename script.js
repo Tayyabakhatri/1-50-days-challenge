39fc1c1943954ba0339d03178cc817e9dcfeec10
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2024-10-02 22:41:52 +0500

    3D boxes complete

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 var container = document.getElementById("container-div");
 var toggleBtn = document.getElementById("btn");
 toggleBtn.addEventListener('click', () => container.classList.toggle('big'))
-function myFUnc() {
+function myFunc() {
     for (var i = 0; i < 4; i++) {
         for (var j = 0; j < 4; j++) {
             var box = document.createElement('div');
-            box.classList.add('boxes')
-            boxes.style.backgroundPosition = `${j * 125}px${i * 125}px`
-            container.appendChild(boxes)
+            box.classList.add('box')
+            box.style.backgroundPosition = `${j * 125}px${i * 125}px`
+            container.appendChild(box)
         }
     }
 }
-myFUnc()
+myFunc()
